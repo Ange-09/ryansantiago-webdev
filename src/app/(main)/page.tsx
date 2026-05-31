22c9c785152d4164db8cf61bfab0c295efe95b65
Ange-09 <santiago.ry.angelo@gmail.com>
2026-05-31 20:47:49 +0800

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,11 +1,16 @@
 import HeroSection from "./home/HeroSection/HeroSection";
 import FeaturedWork from "./home/FeaturedWork/FeaturedWork";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
-      <FeaturedWork />
+    <main className={styles.main}>
+      <section className={styles.section}>
+        <HeroSection />
+      </section>
+      <section className={styles.section}>
+        <FeaturedWork />
+      </section>
     </main>
   );
 }
